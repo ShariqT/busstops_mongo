3acce0006da66ccb0401ec6f321890244670e002
Shariq Torres <shariq.torres@gmail.com>
2015-12-13 13:16:19 -0500

    fixes

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ ADD startup.sh /tmp/startup.sh
 
 RUN chmod +x /tmp/startup.sh
 
-CMD /tmp/startup.sh
+RUN /tmp/startup.sh
 
 #RUN rm -f /tmp/startup.sh
 
--- startup.sh
+++ startup.sh
@@ -1,1 +1,1 @@
-mongoimport --db busstops --collection routes --file /tmp/routes.json
+mongoimport -h 127.0.0.1 --db busstops --collection routes --file /tmp/routes.json
